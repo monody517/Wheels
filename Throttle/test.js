9131db6385f656fb4a701d5fb998b8adabbf6f6b
wenpei <2737716714@qq.com>
2023-01-28 15:46:40 +0800

    重写节流函数

--- Throttle/test.js
+++ Throttle/test.js
@@ -1,14 +1,33 @@
 /*  节流：n秒内只运行一次，若在n秒内重复触发，只有一次触发 */
+var count = 1;
+var container = document.getElementById('container');
 
-function throttled(fn,delay){
-    let timer = null
+function getUserAction() {
+    container.innerHTML = count++;
+};
 
-    return function (...args){
-        if(!timer){
-            timer = setTimeout(()=>{
-                fn.apply(this,args)
-                timer = null
-            },delay)
+container.onmousemove = throttled(getUserAction,3000);
+
+function throttled(fn,wait){
+    var context, args
+    var previous = 0
+    var timeout
+    
+    return function () {
+        context = this
+        args = arguments
+        var now = new Date()
+        // if (now - previous > wait) {
+        //     fn.apply(args, context)
+        //     previous = now
+        // }
+        if (!timeout) {
+            timeout = setTimeout(function () {
+                timeout = null
+                fn.apply(args,context)
+            },wait)
         }
     }
 }
+
+
